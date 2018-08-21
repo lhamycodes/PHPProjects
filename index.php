467578d3bbcde2b31b8ae05f8f5f42cb5fc0aa84
lhamide <flashpointintl@gmail.com>
2018-08-21 17:30:55 +0100

    added and imported jquery.min.js

--- index.php
+++ index.php
@@ -18,83 +18,89 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<div class="page-wrapper flex-row align-items-center">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-5">
-                <div class="card p-4">
-                    <form target="" method="POST" autocomplete="on">
-                        <div class="card-header text-center h4 font-weight-light">
-                            @LhamyCodes<br>
-                            Login Template
-                        </div>
-                        <?php
-                            if(isset($_POST['usernameParam'])){
-                                extract($_POST);
-                                $connect = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-                                $query = mysqli_query($connect, "SELECT * FROM `$tblName` WHERE `username` = '$usernameParam' AND `password` = '$passwordParam'");
-                                if(mysqli_num_rows($query) < 1){
-                                    $failed = "Username / Password Mismatch..";
-                                }
-                                else
-                                {
-                                    while($row = mysqli_fetch_row($query)){
-                                        $userFullName = $row[1];
+    <div class="page-wrapper flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="card p-4">
+                        <form target="" method="POST" autocomplete="on">
+                            <div class="card-header text-center h4 font-weight-light">
+                                @LhamyCodes<br>
+                                Login Template
+                            </div>
+                            <?php
+                                if(isset($_POST['usernameParam'])){
+                                    extract($_POST);
+                                    $connect = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+                                    $query = mysqli_query($connect, "SELECT * FROM `$tblName` WHERE `username` = '$usernameParam' AND `password` = '$passwordParam'");
+                                    if(mysqli_num_rows($query) < 1){
+                                        $failed = "Username / Password Mismatch..";
                                     }
-                                    $success = "Login Successful, Welcome @$userFullname";
+                                    else
+                                    {
+                                        while($row = mysqli_fetch_row($query)){
+                                            $userFullName = $row[1];
+                                        }
+                                        $success = "Login Successful, Welcome $userFullName";
+                                    }
                                 }
-                            }
-                            if(isset($success) || (isset($failed))){
-                                if($success){
-                                    $type = "primary";
-                                    $message = $success;
+                                if(isset($success) || (isset($failed))){
+                                    if($success){
+                                        $type = "primary";
+                                        $message = $success;
+                                    }
+                                    else if ($failed)
+                                    {
+                                        $type = "danger";
+                                        $message = $failed;
+                                    }
+                                    $output =  "<div class='alert alert-dismissible alert-$type'>
+                                                    $message
+
+                                                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                                        <span aria-hidden='true'>&times;</span>
+                                                    </button>
+                                                </div>";
+
+                                    echo $output;
+                                    ?>
+                                        <script type="text/javascript">
+                                        </script>
+                                    <?php
                                 }
-                                else if ($failed)
-                                {
-                                    $type = "danger";
-                                    $message = $failed;
-                                }
-                                $output =  "<div class='alert alert-dismissible alert-$type'>
-                                                $message
+                            ?>
 
-                                                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                                                    <span aria-hidden='true'>&times;</span>
-                                                </button>
-                                            </div>";
-                                echo $output;
-                            }
-                        ?>
-
-                        <div class="card-body py-3">
-                            <div class="form-group">
-                                <label class="form-control-label">Username</label>
-                                <input type="text" name="usernameParam" required class="form-control" placeholder="Username Here">
-                            </div>
-
-                            <div class="form-group">
-                                <label class="form-control-label">Password</label>
-                                <input type="password" name="passwordParam" required class="form-control" placeholder="Password Here">
-                            </div>
-
-                        </div>
-
-                        <div class="card-footer" id="dd">
-                            <div class="row">
-                                <div class="col-6">
-                                    <button type="submit" class="btn btn-primary px-5">Login</button>
+                            <div class="card-body py-3">
+                                <div class="form-group">
+                                    <label class="form-control-label">Username</label>
+                                    <input type="text" name="usernameParam" required class="form-control" placeholder="Username Here">
                                 </div>
 
-                                <div class="col-6 text-right">
-                                    <a href="#" class="btn hlink btn-link">Forgot password?</a>
+                                <div class="form-group">
+                                    <label class="form-control-label">Password</label>
+                                    <input type="password" name="passwordParam" required class="form-control" placeholder="Password Here">
+                                </div>
+
+                            </div>
+
+                            <div class="card-footer" id="dd">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <button type="submit" class="btn btn-primary px-5">Login</button>
+                                    </div>
+
+                                    <div class="col-6 text-right">
+                                        <a href="#" class="btn hlink btn-link">Forgot password?</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        
-                    </form>                
+                            
+                        </form>                
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+<script src="jquery.min.js"></script>
 </html>
